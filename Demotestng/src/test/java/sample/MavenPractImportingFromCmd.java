d0d292fc9901b92619b1487b612f852ca45d0192
Shambhavi Patil <Shambhavi Patil@LAPTOP-OURV1CIL>
2022-07-19 12:08:19 +0530

    2nd commit

--- Demotestng/src/test/java/sample/MavenPractImportingFromCmd.java
+++ Demotestng/src/test/java/sample/MavenPractImportingFromCmd.java
@@ -6,11 +6,13 @@ public class MavenPractImportingFromCmd {
 	
 	@Test
 	public void praticetest1() {
+		System.out.println("hello");
 		System.out.println("TYSS1");
 		String admin = System.getProperty("ADMIN");
 		System.out.println(admin);
 		String password = System.getProperty("MISSIONIMPOSSIBLE");
 		System.out.println(password);
+		
 		
 		
 	}
