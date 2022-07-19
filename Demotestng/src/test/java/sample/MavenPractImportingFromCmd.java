197808eb717b2e75fd54e71d907c89bc9856cd29
Shambhavi Patil <Shambhavi Patil@LAPTOP-OURV1CIL>
2022-07-19 11:46:11 +0530

    1ST COMMIT

---
+++ Demotestng/src/test/java/sample/MavenPractImportingFromCmd.java
@@ -0,0 +1,19 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class MavenPractImportingFromCmd {
+	
+	@Test
+	public void praticetest1() {
+		System.out.println("TYSS1");
+		String admin = System.getProperty("ADMIN");
+		System.out.println(admin);
+		String password = System.getProperty("MISSIONIMPOSSIBLE");
+		System.out.println(password);
+		
+		
+	}
+	
+
+}
---
+++ Demotestng/src/test/java/sample/simpleTest.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class simpleTest {
+	@Test
+public void hi()
+{
+	System.out.println("hi all");
+}
+}
